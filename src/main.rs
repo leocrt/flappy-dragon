da81664016acf5cd79d1726f0e16e68cc9f80ff8
leocrt <60219127+leocrt@users.noreply.github.com>
2020-12-30 23:49:18 -0300

    Inclusão da struct Obstacle

--- src/main.rs
+++ src/main.rs
@@ -87,7 +87,7 @@ impl State {
 
         if let Some(key) = ctx.key{
             match key{
-                VirtualKeyCode::P => self.play(ctx),
+                VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => self.quit_game(ctx),
                 _=>{}
             }
@@ -120,6 +120,45 @@ impl State {
     }
 }
 
+struct Obstacle{
+    x: i32,
+    gap_y: i32,
+    size: i32 
+}
+
+impl Obstacle{
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle{
+            x,
+            gap_y: random.range(20,40),
+            size: i32::max(2,20-score),
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32){
+        let screen_x = self.x - player_x;
+        let half_size = self.size/2;
+
+        for y in 0..self.gap_y - half_size{
+            ctx.set(
+                screen_x,,
+                y,
+                RED,
+                BLACK,
+                to_cp437('|'),
+            );
+        }
+        for y in self.gap_y + half_size..SCREEN_HEIGHT{
+            ctx.set(
+                screen_x,
+                y,
+                RED,
+                BLACK,
+                to_cp437('|')
+            );
+        }
+    }
+}
 impl GameState for State{
     fn tick(&mut self, ctx: &mut BTerm){
         match self.mode{
